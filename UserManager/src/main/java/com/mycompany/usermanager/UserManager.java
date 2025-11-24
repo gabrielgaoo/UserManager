f339c33bf3c1007c699f533a97d36162ae57c902
Gabriel Guimaraes <gabrielplug32@gmail.com>
2025-11-24 19:38:27 -0300

    Hello World!

---
+++ UserManager/src/main/java/com/mycompany/usermanager/UserManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.usermanager;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class UserManager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
